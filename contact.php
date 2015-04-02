7076000c9e187ee703c2c8f9230d2a24d045470f
nuhuxi <mark.keckeis@gmail.com>
2015-04-02 07:57:43 -0700

    Making the contact form work

--- contact.php
+++ contact.php
@@ -42,7 +42,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@themeforest.net";
-$address = "example@themeforest.com";
+$address = "mark.keckeis@gmail.com";
 
 
 // Configuration option.
